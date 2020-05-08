f13e7c89144346a4576539a5dfd1f9e53ee27c66
Tom <desktop-vm-ubuntu>
2020-05-08 17:46:56 +0300

    init project

---
+++ src/main/java/guru/springframework/sfgssm/SfgSsmApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSsmApplication.class, args);
+    }
+
+}
