58f641fbceac7d30b2d4ad05d41b11487c9c2f67
Tom <desktop-vm-ubuntu>
2020-05-11 11:56:38 +0300

    restore state and set it into state machine for objects

--- src/main/java/guru/springframework/sfgssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/sfgssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import guru.springframework.sfgssm.repositories.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,16 +28,33 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentStatus, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentStatus, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentStatus, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentStatus, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentStatus, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentStatus, PaymentEvent> sm = build(paymentId);
         return null;
+    }
+
+    // restore state from db
+    private StateMachine<PaymentStatus, PaymentEvent> build(Long paymentId) {
+        Payment payment = repository.getOne(paymentId);
+        // get state machine for specific object
+        StateMachine<PaymentStatus, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(paymentId));
+        sm.stop();
+        // sets to specific state from db
+        sm.getStateMachineAccessor().doWithAllRegions(sma -> {
+            sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getStatus(), null, null, null));
+        });
+        sm.start();
+        return sm;
     }
 }
